29ae36452a864738769b235e77da3954185fe1f5
Joe Crick <joe@josephcrick.com>
2016-04-26 16:09:12 -0700

    Convert to class component. Add styles

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import SearchBar from './components/search-bar';
+import styles from './main.css';
 
-const App = () => {
-    return (<div>Hi! <SearchBar/></div>);
-};
+class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return (<div>Hi! <SearchBar/></div>);
+    }
+}
 
 ReactDom.render(<App/>, document.body);
