afd1d7d1499d396f84c7e44425b5641c1a02bc1f
Joe Crick <joe@josephcrick.com>
2016-04-26 14:12:31 -0700

    Add basic search bar

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from './main.css';
+import SearchBar from './components/search-bar';
 
 const Hello = React.createClass({
     render() {
-        return <div>Hello {this.props.name}</div>;
+        return (<div>
+                <SearchBar />
+                <br/>
+            Hello
+    </div>);
     }
 })
 
-React.render(<Hello name="Joe"/>, document.body);
+React.render(<Hello/>, document.body);
---
+++ src/components/search-bar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBar = React.createClass({
+    render() {
+        return <input />;
+    }
+});
+
+export default SearchBar;
