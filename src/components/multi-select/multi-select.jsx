a3f1f781401b1e1d220cec14fe95f3e9cb3c7886
Joe Crick <joe@josephcrick.com>
2016-08-02 08:44:22 -0700

    Update styles with Bootstrap upgrade

--- src/components/multi-select/multi-select.jsx
+++ src/components/multi-select/multi-select.jsx
@@ -3,29 +3,30 @@ import {toggleDropDown} from '../../utils/componentEvents';
 
 export default React => observer((props) => {
     return (
-        <klatsch-multi-select>
-              <div className="btn-group">
-                  <button
-                    type="button"
-                    className="btn btn-secondary dropdown-toggle"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                    onClick={event => toggleDropDown(event.target)}>
-                      {props.selectTitle}
-                  </button>
-                  <div className="dropdown-menu">
-                      {props.options.map(function(option) {
-                          return (
-                              <button className='dropdown-item' type="button"
-                                key={option.value}
-                                onClick={event => props.dropDownAction(option)}>
-                                <span className={option.selected ? 'fa fa-check-square' : ''}></span>
-                                <span className={option.selected ? 'selected' : 'unselected'}>{option.label}</span>
-                              </button>
-                          );
-                      })}
-                  </div>
-              </div>
-        </klatsch-multi-select>
+      <klatsch-multi-select>
+            <div className="btn-group">
+                <button
+                  type="button"
+                  className="btn btn-secondary dropdown-toggle"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  onClick={event => toggleDropDown(event.target)}>
+                    {props.selectTitle}
+                    <span className="caret"></span>
+                </button>
+                <div className="dropdown-menu">
+                    {props.options.map(function(option) {
+                        return (
+                            <button className='dropdown-item' type="button"
+                              key={option.value}
+                              onClick={event => props.dropDownAction(option)}>
+                              <span className={option.selected ? 'fa fa-check-square' : ''}></span>
+                              <span className={option.selected ? 'selected' : 'unselected'}>{option.label}</span>
+                            </button>
+                        );
+                    })}
+                </div>
+            </div>
+      </klatsch-multi-select>
     );
 });
