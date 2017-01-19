dde58147d2ecadcbcc4f7332179b3af67ccff72f
Joe Crick <joe@bitovi.com>
2017-01-19 12:19:07 +0100

    (master): Nav logo

--- src/page-templates/master-template/nav/nav-logo/nav-logo.jsx
+++ src/page-templates/master-template/nav/nav-logo/nav-logo.jsx
@@ -1,15 +1,30 @@
-export default React => (props) => {
-    return (
-        <klatsch-nav-logo>
-          <div className="navbar-header">
-              <button type="button" className="navbar-toggle"
-                data-toggle="collapse" data-target="#klatsch-nav-bar"
-                aria-expanded="false">
-                  <span className="sr-only">Toggle navigation</span>
-                  <span className="glyphicon glyphicon-menu-hamburger"></span>
-              </button>
-              <a className="navbar-brand" href="#">Klatsch</a>
-          </div>
-        </klatsch-nav-logo>
-      );
-    }
+export default React => {
+
+	const {string} = React.PropTypes;
+
+	return function NavLogo(props) {
+
+		NavLogo.PropTypes = {
+			toggleNavText: string
+		};
+
+		return {
+			props,
+			render() {
+				return (
+					<klatsch-nav-logo>
+						<div className="navbar-header">
+							<button type="button" className="navbar-toggle"
+									data-toggle="collapse" data-target="#klatsch-nav-bar"
+									aria-expanded="false">
+								<span className="sr-only">{props.toggleNavText}</span>
+								<span className="glyphicon glyphicon-menu-hamburger"></span>
+							</button>
+							<a className="navbar-brand" href="#">Klatsch</a>
+						</div>
+					</klatsch-nav-logo>
+				);
+			}
+		}
+	}
+}
