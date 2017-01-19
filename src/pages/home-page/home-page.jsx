73635924bc3f49e1f769f2418dff59acfdc91aa7
Joe Crick <joe@bitovi.com>
2017-01-19 11:51:24 +0100

    (master): Refactor home page

--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -3,24 +3,40 @@ import createSignUpBar from './sign-up-bar/sign-up-bar';
 import createLoginBar from './login-bar/login-bar';
 import createTagLine from './tag-line/tag-line';
 
-export default React => (props) => {
+export default React => {
 
-    let LoginBar = createLoginBar(React);
-    let TagLine = createTagLine(React);
-    let SignUpBar = createSignUpBar(React);
+	const {string} = React.PropTypes;
 
-    return (
-        <klatsch-home-page>
-            <div className="home-bg">
-                <div className="container">
-                    <div className="home-logo"></div>
-                    <div>
-                        <LoginBar store={HomeStore} authStore={props.authStore}/>
-                        <SignUpBar store={HomeStore}/>
-                        <TagLine tagLine={HomeStore.tagLine}/>
-                    </div>
-                </div>
-            </div>
-        </klatsch-home-page>
-    )
+	return function HomePage(props) {
+
+		let LoginBar = createLoginBar(React);
+		let TagLine = createTagLine(React);
+		let SignUpBar = createSignUpBar(React);
+
+		HomePage.PropTypes = {
+			tagLine: string
+		};
+
+		return {
+
+			props,
+
+			render() {
+				return (
+					<klatsch-home-page>
+						<div className="home-bg">
+							<div className="container">
+								<div className="home-logo"></div>
+								<div>
+									<LoginBar store={HomeStore} authStore={props.authStore}/>
+									<SignUpBar store={HomeStore}/>
+									<TagLine tagLine={HomeStore.tagLine}/>
+								</div>
+							</div>
+						</div>
+					</klatsch-home-page>
+				)
+			}
+		}
+	}
 };
