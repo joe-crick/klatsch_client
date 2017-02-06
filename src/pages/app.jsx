4180176de2fae10908e80b2e5164d6be0f3899f7
Joe Crick <joe@bitovi.com>
2017-02-06 09:35:50 +0100

    (master): Convert constructor function to class.

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -5,33 +5,33 @@ import {connect} from 'react-redux';
 
 const {object} = React.PropTypes;
 
-function App(props) {
 
-	App.PropTypes = {
-		authStore: object
-	};
-
-	return {
-		props,
-
-		render() {
-			if (props.authStore.get('isAuthenticated')) {
-				return (
-					<ReactRedirect location='/dashboard'>
-						<this.props.activeRouteHandler />
-					</ReactRedirect>);
-			} else {
-				return (
-					<HomePage
-						authStore={props.authStore}
-						homeStore={props.homeStore}/>
-				);
-			}
-		}
-
+class App extends React.Component {
+	constructor(props) {
+		super(props);
 	}
 
+	render() {
+		const props = this.props;
+
+		if (props.authStore.get('isAuthenticated')) {
+			return (
+				<ReactRedirect location='/dashboard'>
+					<this.props.activeRouteHandler />
+				</ReactRedirect>);
+		} else {
+			return (
+				<HomePage
+					authStore={props.authStore}
+					homeStore={props.homeStore}/>
+			);
+		}
+	}
 }
+
+App.propTypes = {
+	authStore: object
+};
 
 function mapPropsToState(state) {
 	return {
