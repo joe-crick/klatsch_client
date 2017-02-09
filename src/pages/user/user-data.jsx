094ee0c54582ed170b7b12cbb7f8d9b74ae7f935
Joe Crick <joe@bitovi.com>
2017-02-09 09:16:45 +0100

    (master): Update user-data to new format

--- src/pages/user/user-data.jsx
+++ src/pages/user/user-data.jsx
@@ -1,46 +1,44 @@
-import multiSelectFactory from '../../components/multi-select/multi-select';
+import React from 'react';
 
-export default React => {
+import MultiSelect from '../../components/multi-select/multi-select';
 
-	const {string, array, func} = React.PropTypes;
 
-	return function UserData(props) {
+const {string, array, func} = React.PropTypes;
 
-		UserData.PropTypes = {
-			userNameLabel: string,
-			genderLabel: string,
-			genderOptions: array,
-			selectGender: func,
-			genderSelectLabel: string,
-			ageLabel: string,
-			zipCode: string
-		};
+export default function UserData(props) {
 
-		let MultiSelect = multiSelectFactory(React);
+	UserData.PropTypes = {
+		userNameLabel: string,
+		genderLabel: string,
+		genderOptions: array,
+		selectGender: func,
+		genderSelectLabel: string,
+		ageLabel: string,
+		zipCode: string
+	};
 
-		return (
-			<klatsch-user-data>
-				<form>
-					<fieldset className="form-group">
-						<label htmlFor="user-name">{props.userNameLabel}</label>
-						<input id="user-name" className="form-control" type="text"></input>
-					</fieldset>
-					<fieldset className="form-group">
-						<label>{props.genderLabel}
-							<MultiSelect options={props.genderOptions} dropDownAction={props.dropDownAction}
-										 selectTitle={props.genderSelectLabel}/>
-						</label>
-					</fieldset>
-					<fieldset className="form-group">
-						<label htmlFor="age">{props.ageLabel}</label>
-						<input id="age" className="form-control" type="number"></input>
-					</fieldset>
-					<fieldset className="form-group">
-						<label htmlFor="zip">{props.zipCode}</label>
-						<input id="zip" className="form-control" type="text"></input>
-					</fieldset>
-				</form>
-			</klatsch-user-data>
-		);
-	}
-};
+	return (
+		<klatsch-user-data>
+			<form>
+				<fieldset className="form-group">
+					<label htmlFor="user-name">{props.userNameLabel}</label>
+					<input id="user-name" className="form-control" type="text"></input>
+				</fieldset>
+				<fieldset className="form-group">
+					<label>{props.genderLabel}
+						<MultiSelect options={props.genderOptions} dropDownAction={props.dropDownAction}
+									 selectTitle={props.genderSelectLabel}/>
+					</label>
+				</fieldset>
+				<fieldset className="form-group">
+					<label htmlFor="age">{props.ageLabel}</label>
+					<input id="age" className="form-control" type="number"></input>
+				</fieldset>
+				<fieldset className="form-group">
+					<label htmlFor="zip">{props.zipCode}</label>
+					<input id="zip" className="form-control" type="text"></input>
+				</fieldset>
+			</form>
+		</klatsch-user-data>
+	);
+}
