40a54c87e9d8646464bb6ca4a8d6779b469a6473
Joe Crick <joe@bitovi.com>
2017-02-09 08:57:43 +0100

    (master): Update to webpack 2.

--- webpack.config.js
+++ webpack.config.js
@@ -3,46 +3,41 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 
 const sassLoaders = [
-    'css-loader',
-    'postcss-loader',
-    'sass-loader?sourceMap&indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, './src')
+	'css-loader',
+	'postcss-loader',
+	'sass-loader?sourceMap&indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, './src')
 ];
 
 module.exports = {
-    entry: [
-        './src/app.js'
-    ],
-    output: {
-        path: __dirname + '/build',
-        publicPath: '/build',
-        filename: 'bundle.js'
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            {
-                exclude: /node_modules/,
-                loader: 'babel'
-            },
-            {
-                test: /\.sass$/,
-                loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('[name].css')
-    ],
-    postcss: [
-        autoprefixer({
-            browsers: ['last 2 versions']
-        })
-    ],
-    resolve: {
-        extensions: ['', '.js', '.jsx', '.sass']
-    },
-    devServer: {
-        historyApiFallback: true,
-        contentBase: './'
-    }
+	entry: [
+		'./src/app.js'
+	],
+	output: {
+		path: __dirname + '/build',
+		publicPath: '/build',
+		filename: 'bundle.js'
+	},
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.sass$/,
+				use: ExtractTextPlugin.extract({fallback: 'style-loader', loader: sassLoaders.join('!')})
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin('[name].css')
+	],
+	resolve: {
+		extensions: ['.js', '.jsx', '.sass']
+	},
+	devServer: {
+		historyApiFallback: true,
+		contentBase: './'
+	}
 };
