cb1c24b9c29cdaff2bc4fbb8af6e828b9735a2bf
Joe Crick <joe@bitovi.com>
2017-02-11 17:59:25 +0100

    (master): Placeholders under footer icons

--- src/pages/home-page/home-bottom-nav/home-bottom-nav.jsx
+++ src/pages/home-page/home-bottom-nav/home-bottom-nav.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default props => (
 	<klatsch-home-bottom-nav>
 		<ul>
-			<li><img src="/src/img/abc.svg" className="footer-image"/></li>
-			<li><img src="/src/img/duckling.svg" className="footer-image"/></li>
-			<li><img src="/src/img/childhood.svg" className="footer-image"/></li>
+			<li><img src="/src/img/abc.svg" className="footer-image"/> <div>Infants</div> </li>
+			<li><img src="/src/img/duckling.svg" className="footer-image"/> <div>Toddlers</div></li>
+			<li><img src="/src/img/childhood.svg" className="footer-image"/> <div>School Age</div></li>
 		</ul>
 	</klatsch-home-bottom-nav>
 );
