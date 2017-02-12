4142e779b03bebfc5444e63b952941498254d4f3
Joe Crick <joe@bitovi.com>
2017-02-12 08:42:12 +0100

    (master): Make bottom nav responsive.

--- src/pages/home-page/home-bottom-nav/home-bottom-nav.jsx
+++ src/pages/home-page/home-bottom-nav/home-bottom-nav.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 export default props => (
-	<klatsch-home-bottom-nav>
-		<ul>
-			<li><img src="/src/img/abc.svg" className="footer-image"/> <div>Infants</div> </li>
-			<li><img src="/src/img/duckling.svg" className="footer-image"/> <div>Toddlers</div></li>
-			<li><img src="/src/img/childhood.svg" className="footer-image"/> <div>School Age</div></li>
-		</ul>
-	</klatsch-home-bottom-nav>
+	<div className="row klatsch-home-bottom-nav">
+		<div className="col">
+			<img src="/src/img/abc.svg" className="footer-image"/>
+			<div>Infants</div>
+		</div>
+		<div className="col">
+			<img src="/src/img/duckling.svg" className="footer-image"/>
+			<div>Toddlers</div>
+		</div>
+		<div className="col">
+			<img src="/src/img/childhood.svg" className="footer-image"/>
+			<div>School Age</div>
+		</div>
+	</div>
 );
