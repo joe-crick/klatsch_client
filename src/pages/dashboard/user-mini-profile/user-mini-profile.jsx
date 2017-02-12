65862c756f68fe284c4b8b8d72b63545600187a1
Joe Crick <joe@bitovi.com>
2017-02-12 12:33:15 +0100

    ((HEAD detached from fa3378b)): Format action center, and question teaser

--- src/pages/dashboard/user-mini-profile/user-mini-profile.jsx
+++ src/pages/dashboard/user-mini-profile/user-mini-profile.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 
 const UserMiniProfile = props => {
 	return (
-		<div className="profile_info">
-			<div className="username">
-				<a href="#" className="name">Monkey Chunk</a>
+		<div className="user-mini-profile">
+			<a href="#" className="pull-left">
+				<img src="#"/>
+			</a>
+			<div className="user-data pull-left">
+				<div className="username">
+					<a href="#" className="mini-profile-name">Monkey Chunk</a>
+				</div>
+				<div className="userinfo">
+					<span className="age">kids: 2, 5</span>
+					<span> | </span>
+					<span className="location">Seattle, WA</span>
+				</div>
 			</div>
-			<div className="userinfo">
-				<span className="age">kids: 2, 5</span>
-				<span> | </span>
-				<span className="location">Seattle, WA</span>
+			<div className="pull -left">
+				<a href="#" className="action-center-mail-notification"><span className="fa fa-envelope"/></a>
 			</div>
+			<div className="clearfix"/>
 		</div>
 	);
 };
