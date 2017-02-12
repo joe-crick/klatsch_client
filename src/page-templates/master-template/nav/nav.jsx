a086509243eee674dd1bdf0cf438d176eeb03b91
Joe Crick <joe@bitovi.com>
2017-02-12 10:32:49 +0100

    (master): Add nav-logo class to app-level sass. Start update on dashboard nav-bar

--- src/page-templates/master-template/nav/nav.jsx
+++ src/page-templates/master-template/nav/nav.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import NavLogo from './nav-logo/nav-logo';
 import QuickLinks from './quick-links/quick-links';
 import RightNav from './right-nav/right-nav';
 
 export default props => (
 	<klatsch-nav>
-		<nav className="navbar navbar-default navbar-fixed-top">
-			<div className="container-fluid">
-				<NavLogo/>
-				<div className="collapse navbar-collapse" id="klatsch-nav-bar">
-					<QuickLinks />
-					<RightNav />
-				</div>
+		<nav className="navbar fixed-top navbar-toggleable-md navbar-light bg-faded home-nav">
+			<div className="home-logo"></div>
+			<div className="collapse navbar-collapse" id="klatsch-nav-bar">
+				<QuickLinks />
+				<RightNav />
 			</div>
 		</nav>
 	</klatsch-nav>
--- src/pages/home-page/home-nav/home-nav.jsx
+++ src/pages/home-page/home-nav/home-nav.jsx
@@ -5,15 +5,6 @@ import LoginButton from '../login-bar/login-bar';
 const HomeNav = props => {
 	return (
 		<nav className="navbar fixed-top navbar-toggleable-md navbar-light bg-faded home-nav">
-			<button className="navbar-toggler navbar-toggler-right"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
-					aria-label="Toggle navigation">
-				<span className="navbar-toggler-icon"/>
-			</button>
 			<div className="home-logo"></div>
 
 			<div className="collapse navbar-collapse" id="navbarSupportedContent">
