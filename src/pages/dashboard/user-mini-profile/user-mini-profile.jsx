4a631f185f3d6b5c6fa09f3b2195a48d1ed65550
Joe Crick <joe@bitovi.com>
2017-02-12 12:41:55 +0100

    (master): Add sample image.

--- src/pages/dashboard/user-mini-profile/user-mini-profile.jsx
+++ src/pages/dashboard/user-mini-profile/user-mini-profile.jsx
@@ -4,14 +4,14 @@ const UserMiniProfile = props => {
 	return (
 		<div className="user-mini-profile">
 			<a href="#" className="pull-left">
-				<img src="#"/>
+				<img src="http://www.avatarsdb.com/avatars/dust_of_the_past.gif" className="mini-profile-image"/>
 			</a>
 			<div className="user-data pull-left">
 				<div className="username">
 					<a href="#" className="mini-profile-name">Monkey Chunk</a>
 				</div>
 				<div className="userinfo">
-					<span className="age">kids: 2, 5</span>
+					<span className="age">kids: 2yrs, 5yrs</span>
 					<span> | </span>
 					<span className="location">Seattle, WA</span>
 				</div>
