98a5ba1a685efc1e79a4812af9973db3452611f9
Joe Crick <joe@bitovi.com>
2017-02-12 07:50:20 +0100

    (master): Format footer and bottom nav

--- src/pages/home-page/home-footer/home-footer.jsx
+++ src/pages/home-page/home-footer/home-footer.jsx
@@ -6,12 +6,12 @@ const homeFooter = props => {
 			<div className="signup-footer">
 				<div className="footer">
 					<ul className="footer-nav">
-						<li><a className="white-link" href="/mobile" target="_blank"> Apps </a></li>
+						<li><a className="white-link" href="/apps" target="_blank"> Apps </a></li>
 						<li><a className="white-link" href="/press" target="_blank"> Press </a></li>
 						<li><a className="white-link" href="/about" target="_blank"> About </a></li>
 						<li><a className="white-link" href="/support" target="_blank"> Support </a></li>
 						<li><a className="white-link" href="/legal/privacy" target="_blank"> Privacy Policy </a></li>
-						<li><a className="white-link" href="/legal/terms" target="_blank"> Terms and Conditions </a></li>
+						<li><a className="white-link" href="/legal/t-and-c" target="_blank"> Terms and Conditions </a></li>
 					</ul>
 					<div className="footer-copyright"> © Klatsch 2017</div>
 					<div className="footer-social">
