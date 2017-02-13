7eff7a2f0658b729c6ab54a8567ad0ce71ff2f9e
Joe Crick <joe@bitovi.com>
2017-02-13 13:43:11 +0100

    (master): Add overview summary

--- src/pages/profile/profile-header/profile-header-summary/profile-header-summary.jsx
+++ src/pages/profile/profile-header/profile-header-summary/profile-header-summary.jsx
@@ -4,8 +4,52 @@ const ProfileHeaderSummary = props => {
 	return (
 	<div className="klatsch-profile-header-summary container">
 		<div className="row">
+			<div className="col summary-title">
+				Location:
+			</div>
 			<div className="col">
-				Hello World
+				Seattle, WA (within 10 miles)
+			</div>
+		</div>
+		<div className="row">
+			<div className="col summary-title">
+				Kids:
+			</div>
+			<div className="col">
+				Boy (5), Girl (3)
+			</div>
+		</div>
+		<div className="row">
+			<div className="col summary-title">
+				Family Status:
+			</div>
+			<div className="col">
+				Couple
+			</div>
+		</div>
+		<div className="row">
+			<div className="col summary-title">
+				Faith:
+			</div>
+			<div className="col">
+				Not Mentioned
+			</div>
+		</div>
+		<div className="row">
+			<div className="col summary-title">
+				Interests:
+			</div>
+			<div className="col family-interests vertical-scrollable-section">
+				<ul>
+					<li>Organic</li>
+					<li>Home school</li>
+					<li>Theater</li>
+					<li>Unschooling</li>
+					<li>Free-range kids</li>
+					<li>Model cars</li>
+					<li>Horses</li>
+					<li>Science</li>
+				</ul>
 			</div>
 		</div>
 	</div>
