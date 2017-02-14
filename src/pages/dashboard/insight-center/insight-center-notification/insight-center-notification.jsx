9ceb04ff0bfe68ec0c96d5fc3c7e07c3ec50a311
Joe Crick <joe@bitovi.com>
2017-02-14 09:07:27 +0100

    (master): Fix display of time on profile for iphone5

--- src/pages/dashboard/insight-center/insight-center-notification/insight-center-notification.jsx
+++ src/pages/dashboard/insight-center/insight-center-notification/insight-center-notification.jsx
@@ -8,12 +8,12 @@ const InsightCenterNotification = props => {
 	return (
 		<div className="container insight-center-message">
 			<div className="row highlight-row">
-				<div className="col-sm-12 col-md-7 col-lg-7">
+				<div className="col-sm-10 col-md-7 col-lg-7">
 					<UserMiniProfile/>
 				</div>
 				<div className="col-sm-0"/>
-				<div className="col time-on-profile">
-					Time on Profile: {timeOnProfile}
+				<div className="col-sm-1 time-on-profile">
+					<span className="time-on-profile-label">Time on Profile:</span> {timeOnProfile}
 				</div>
 				<div className="clearfix"/>
 			</div>
