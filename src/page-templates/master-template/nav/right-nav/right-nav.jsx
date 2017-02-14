8dda65cdd43b8f87cdb6010f2a3753c07d8b6c8c
Joe Crick <joe@bitovi.com>
2017-02-14 08:54:38 +0100

    (master): Remove visitors from the top nav

--- src/page-templates/master-template/nav/right-nav/right-nav.jsx
+++ src/page-templates/master-template/nav/right-nav/right-nav.jsx
@@ -4,11 +4,6 @@ import UserMenu from '../user-menu/user-menu';
 export default props => (
 	<ul className="navbar-nav mr-auto mt-2 mt-lg-0 klatsch-right-nav">
 		<li className="nav-item">
-			<a href="#" title="Visitors">
-				<span className="fa fa-eye" aria-hidden="true"/>
-			</a>
-		</li>
-		<li className="nav-item">
 			<a href="#" title="Messages">
 				<span className="fa fa-comment" aria-hidden="true"/>
 			</a>
