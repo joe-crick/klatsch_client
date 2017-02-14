6dc7b8ae9033fe0b2aa05ae388bb2745e23d8c9a
Joe Crick <joe@bitovi.com>
2017-02-14 07:45:21 +0100

    (master): Incremental commit on formating and contents of basic data input

--- src/pages/edit-profile/edit-profile-body/basic-content/basic-content.jsx
+++ src/pages/edit-profile/edit-profile-body/basic-content/basic-content.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import {WithContext as ReactTags} from 'react-tag-input';
+import {Editor, EditorState} from 'draft-js';
+
+const tags = [];
+const editorState = EditorState.createEmpty();
+const bogusFunction = bogus => bogus;
 
 const BasicContent = props => {
 	return (
-		<div className="klatsch-edit-profile-basic-content">
-
+		<div className="klatsch-edit-profile-basic-content container">
+			<h4 className="basic-content-header">Basic Content</h4>
+			<form className="basic-content-form">
+				<div className="form-group">
+					<div className="row">
+						<div className="col-lg-5">
+							<label htmlFor="email">Email address</label>
+							<input type="email" className="form-control" id="email" aria-describedby="emailHelp"
+								   placeholder="Enter email"/>
+							<small id="emailHelp" className="form-text text-muted">We'll never share your email with
+								anyone
+								else.
+							</small>
+						</div>
+						<div className="col-lg-5">
+							<label htmlFor="password">Password</label>
+							<input type="password" className="form-control" id="password" placeholder="Password"/>
+						</div>
+					</div>
+				</div>
+				<div className="form-group">
+					<label>Bio</label>
+					<Editor editorState={editorState}/>
+				</div>
+				<div className="form-group">
+					<label>Family </label>
+					<ReactTags tags={tags}
+							   handleDelete={bogusFunction}
+							   handleAddition={bogusFunction}
+							   handleDrag={bogusFunction}/>
+				</div>
+				<div className="form-group">
+					<label>Interests</label>
+					<ReactTags tags={tags}
+							   handleDelete={bogusFunction}
+							   handleAddition={bogusFunction}
+							   handleDrag={bogusFunction}/>
+				</div>
+			</form>
 		</div>
 	);
 };
--- src/pages/edit-profile/edit-profile-body/edit-profile-body.jsx
+++ src/pages/edit-profile/edit-profile-body/edit-profile-body.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import BasicContent from './basic-content/basic-content';
 
 const ProfileBody = props => {
 	return (
-		<div className="klatsch-profile-body container">
+		<div className="klatsch-edit-profile-body container">
 			<h4 className="klatsch-profile-body-header">Profile</h4>
 			<nav className="nav nav-pills flex-column flex-sm-row">
 				<a className="flex-sm-fill text-sm-center nav-link active" data-toggle="tab" role="tab" href="#basics">Basics</a>
@@ -10,7 +11,7 @@ const ProfileBody = props => {
 			</nav>
 			<div className="tab-content">
 				<div className="tab-pane active" id="basics" role="tabpanel">
-					Basic Content
+					<BasicContent/>
 				</div>
 				<div className="tab-pane" id="photos" role="tabpanel">
 					Photos
