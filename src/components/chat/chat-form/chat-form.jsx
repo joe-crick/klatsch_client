29a455d1426c6dce5fa2f19931404d7bac01a9e2
Joe Crick <joe@bitovi.com>
2017-02-16 08:58:38 +0100

    (master): Add silent send button

--- src/components/chat/chat-form/chat-form.jsx
+++ src/components/chat/chat-form/chat-form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ToolTip from '../../tooltip/tooltip';
 
 const ChatForm = props => {
   return (
@@ -14,6 +15,17 @@ const ChatForm = props => {
                   <span className="input-group-addon send-message"><span
                     className="send-message fa fa-share-square"/></span>
                 </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col send-silent">
+                <button className="btn btn-klatsch-secondary">
+                  Silent Send
+                </button>
+                <ToolTip>
+                  When you send a message in "silent mode" it's like sending an email. The recipient gets a notification
+                  in their action center that they've received a message, but their chat window does not open.
+                </ToolTip>
               </div>
             </div>
           </div>
